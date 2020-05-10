b534cd61d628c9530e1f3b331d9620254268747c
Matt Kocubinski <mkocubinski@gmail.com>
2020-05-10 14:28:21 -0400

    Exercise 1.15

--- ch1-2.scm
+++ ch1-2.scm
@@ -94,4 +94,42 @@
   (= (fib n)
      (fib-approx n)))
 
-;; 
+;; Exercise 1.14
+
+(define (count-change amount)
+  (cc amount 5))
+
+(define (cc amount kinds-of-coins)
+  (println amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (else (+ (cc amount
+                     (- kinds-of-coins 1))
+                 (cc (- amount
+                        (first-denomination kinds-of-coins))
+                     kinds-of-coins)))))
+
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)))
+
+;; Exercise 1.15
+
+(define (cube x) (* x x x))
+
+(define (sine angle)
+  (define (p x)
+    (println x)
+    (- (* 3 x) (* 4 (cube x))))
+  (define (sine* angle)
+    (if (not (> (abs angle) 0.1))
+	angle
+	(p (sine* (/ angle 3.0)))))
+  (sine* angle))
+
+(define logB 
+  (lambda (x B) 
+    (/ (log x) (log B))))
