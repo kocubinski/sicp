f03f0a3ea464ce1c12b7960ecfa987f65e5c84b7
Matt Kocubinski <mkocubinski@gmail.com>
2020-05-20 23:27:26 -0400

    Simpsons rule, begin ch 1.3

---
+++ ch1-3.scm
@@ -0,0 +1,30 @@
+;; Exercise 1.29
+(define (even? n) (= 0 (remainder n 2)))
+(define (cube x) (* x x x))
+(define (inc n) (+ n 1))
+
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (integral f a b dx)
+  (define (add-dx x) (+ x dx))
+  (* (sum f (+ a (/ dx 2.0)) add-dx b)
+     dx))
+
+(integral cube 0 2 0.01)
+
+(define (simpsons-rule f a b n)
+  (let ((h (/ (- b a) n)))
+    (define (term k) (* (cond ((= k 0) 1)
+			      ((= k n) 1)
+			      ((even? k) 2)
+			      (else 4))
+			(f (+ a (* k h)))))
+    (* (/ h 3)
+       (sum term 0 inc n))))
+
+(simpsons-rule cube 0 1 100)
+;; => 1/4
