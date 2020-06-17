57d14d7df8c4202950c771af5206f71cebac5cac
Matt Kocubinski <mkocubinski@gmail.com>
2020-06-16 21:12:21 -0400

    Exercise 2.30

--- ch2-2.scm
+++ ch2-2.scm
@@ -281,3 +281,19 @@
 
 ;; modified selectors from cadr -> cdr
 ;; modified predicate from list? -> pair?
+
+;; Exercise 2.30
+
+(define (square-tree tree)
+  (map (lambda (x)
+	 (if (pair? x)
+	     (square-tree x)
+	     (* x x)))
+       tree))
+
+(square-tree
+ (list 1
+       (list 2 (list 3 4) 5)
+       (list 6 7)))
+;; (1 (4 (9 16) 25) (36 49))
+
