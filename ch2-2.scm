3b9c7af8f02650d8430358988c494bb742d962f7
Matt Kocubinski <mkocubinski@gmail.com>
2020-06-23 22:36:19 -0400

    Exercise 2.33

--- ch2-2.scm
+++ ch2-2.scm
@@ -334,5 +334,28 @@
 
 (subsets (list 1 2 3))
 
+;; Exercise 2.33
 
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
 
+(accumulate + 0 (list 4 4 4))
+
+(define (map p sequence)
+  (accumulate (lambda (x y) (cons (p x) y)) nil sequence))
+
+(map (lambda (n) (* 2 n)) (list 1 2 3 4 5))
+;;Value: (2 4 6 8 10)
+
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+(append (list 1 2 3) (list 4 5 6))
+
+(define (length sequence)
+  (accumulate (lambda (x y) (+ 1 y)) 0 sequence))
+
+(length (list 1 2 3 4 5 6))
